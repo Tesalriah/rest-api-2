8c7ab333301a5118e992b2cbcb0e1c894b387a33
Tesalriah <proella8@gmail.com>
2026-06-04 16:14:29 +0900

    ~62

--- build.gradle.kts
+++ build.gradle.kts
@@ -35,6 +35,10 @@ dependencies {
     implementation("org.springframework.boot:spring-boot-h2console")
     // Spring doc
     implementation("org.springdoc:springdoc-openapi-starter-webmvc-ui:2.8.9")
+    // jwt
+    implementation("io.jsonwebtoken:jjwt-api:0.12.6")
+    runtimeOnly("io.jsonwebtoken:jjwt-impl:0.12.6")
+    runtimeOnly("io.jsonwebtoken:jjwt-jackson:0.12.6")
 }
 
 tasks.withType<Test> {
